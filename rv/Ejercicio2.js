3a903f0439777a75673ebf6bca45f61821b1989c
AngelVillalobos <langelvilla22@gmail.com>
2016-08-20 15:41:27 -0500

    Update Ejercicio2.js

--- rv/Ejercicio2.js
+++ rv/Ejercicio2.js
@@ -5,13 +5,13 @@ var renderizador=new THREE.WebGLRenderer();
   renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
   document.body.appendChild(renderizador.domElement);
 var forma1=new THREE.SphereGeometry(1,32,32);
-var material1=new THREE.MeshNormalMaterial();
+var material1=new THREE.MeshBasicMaterial({color:0x0C6E15});
 var esfera=new THREE.Mesh(forma1,material1);
 var forma2=new THREE.CylinderGeometry(0.5,0.5,3,64);
-var material2=new THREE.MeshNormalMaterial();
+var material2=new THREE.MeshBasicMaterial({color:0x61380B});
 var cilindro=new THREE.Mesh(forma2,material2);
 cilindro.rotateX(-Math.PI/8);
-cilindro.position.set(1,1,3);
+cilindro.position.set(0.6,0.6,3);
 escena.add(esfera)
 escena.add(cilindro)
 renderizador.render(escena,camara)
