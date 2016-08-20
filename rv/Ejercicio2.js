67aebd386e8d3fd71a256f6a2e66374dc79c1d14
AngelVillalobos <langelvilla22@gmail.com>
2016-08-20 14:45:38 -0500

    Update Ejercicio2.js

--- rv/Ejercicio2.js
+++ rv/Ejercicio2.js
@@ -4,7 +4,7 @@ var camara=new THREE.PerspectiveCamera();
 var renderizador=new THREE.WebGLRenderer();
   renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
   document.body.appendChild(renderizador.domElement);
-var forma1=new THREE.SphereGeometry(5,10,10);
+var forma1=new THREE.SphereGeometry(1,32,32);
 var material1=new THREE.MeshNormalMaterial();
 var esfera=new THREE.Mesh(forma1,material1);
 escena.add(esfera)
