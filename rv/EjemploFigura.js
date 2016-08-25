bf793010369419f23d9ea452c4d3eb5dfc990894
AngelVillalobos <langelvilla22@gmail.com>
2016-08-25 15:10:12 -0500

    Update EjemploFigura.js

--- rv/EjemploFigura.js
+++ rv/EjemploFigura.js
@@ -11,6 +11,6 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z=100;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
-document.body.appendChild(Renderizador.domElement);
+document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
 
