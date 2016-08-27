a2e7fbc5a948a1fcef3bd9cd2919935fc03156f0
AngelVillalobos <langelvilla22@gmail.com>
2016-08-27 15:52:01 -0500

    Update Ejercicio3.js

--- rv/Ejercicio3.js
+++ rv/Ejercicio3.js
@@ -9,9 +9,20 @@ forma.vertices.push(new THREE.Vector3(-1,1,1),
                     new THREE.Vector3(1,-1,-1),
                     new THREE.Vector3(1,-1,1));
 
-forma.faces.push(new THREE.Face3(0,2,1));
+forma.faces.push(new THREE.Face3(0,2,1),
+                new THREE.Face3(0,3,2),
+                new THREE.Face3(0,4,3),
+                new THREE.Face3(4,7,3),
+                new THREE.Face3(7,4,5),
+                new THREE.Face3(7,5,6),
+                new THREE.Face3(6,5,2),
+                new THREE.Face3(6,1,2),
+                new THREE.Face3(2,3,6),
+                new THREE.Face3(2,7,6),
+                new THREE.Face3(4,0,1),
+                new THREE.Face3(4,1,5));
 
-//forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
+forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
 
 forma.computeFaceNormals();
 
