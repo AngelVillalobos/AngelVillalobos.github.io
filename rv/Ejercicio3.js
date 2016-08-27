df38cdfc25558bb59d8f7cb508d67ebb767f523a
AngelVillalobos <langelvilla22@gmail.com>
2016-08-27 16:41:14 -0500

    Update Ejercicio3.js

--- rv/Ejercicio3.js
+++ rv/Ejercicio3.js
@@ -23,7 +23,7 @@ forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh(forma,material);
-malla.rotateX(-Math.PI/6);
+malla.rotateX(-Math.PI/3);
 malla.rotateY(Math.PI/12);
 malla.rotateZ(-Math.PI/12);
 
