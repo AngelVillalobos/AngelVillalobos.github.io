2001997ff7d10a644a0137d8736556548493b493
AngelVillalobos <langelvilla22@gmail.com>
2016-08-27 16:17:21 -0500

    Update Ejercicio3.js

--- rv/Ejercicio3.js
+++ rv/Ejercicio3.js
@@ -12,7 +12,7 @@ forma.vertices.push(new THREE.Vector3(-1,1,1),
 forma.faces.push(new THREE.Face3(0,1,2),
                 new THREE.Face3(0,2,3),
                 new THREE.Face3(0,4,3),
-                new THREE.Face3(4,3,7));
+                new THREE.Face3(7,3,4));
 
 forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
 
