d7660dda20fb5c1aa9270e94c184a54a0aa89328
AngelVillalobos <langelvilla22@gmail.com>
2016-08-27 16:35:31 -0500

    Update Ejercicio3.js

--- rv/Ejercicio3.js
+++ rv/Ejercicio3.js
@@ -16,7 +16,7 @@ forma.faces.push(new THREE.Face3(0,1,2),
                 new THREE.Face3(7,4,5),
                 new THREE.Face3(6,7,5),
                 new THREE.Face3(2,5,6),
-                new THREE.Face3(1,6,2));
+                new THREE.Face3(1,5,2));
 
 forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
 
@@ -26,7 +26,7 @@ var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh(forma,material);
 malla.rotateX(-Math.PI/6);
 malla.rotateY(Math.PI/5);
-malla.rotateZ(Math.PI/8);
+
 
 var escena = new THREE.Scene();
 escena.add(malla);
