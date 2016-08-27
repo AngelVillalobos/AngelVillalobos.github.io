9437409852d92aca2b7df3b025ca3232a58b115a
AngelVillalobos <langelvilla22@gmail.com>
2016-08-27 15:21:40 -0500

    Create Ejercicio3.js

---
+++ rv/Ejercicio3.js
@@ -0,0 +1,35 @@
+var forma=new THREE.Geometry();
+
+forma.vertices.push(new THREE.Vector3(-1,1,1),
+                    new THREE.Vector3(-1,1,-1),
+                    new THREE.Vector3(1,1,-1),
+                    new THREE.Vector3(1,1,1),
+                    new THREE.Vector3(-1,-1,1),
+                    new THREE.Vector3(-1,-1,-1),
+                    new THREE.Vector3(1,-1,-1),
+                    new THREE.Vector3(1,-1,1));
+
+forma.faces.push(new THREE.Face3(3,2,1),
+                new THREE.Face3(3,1,0),
+                new THREE.Face3(3,0,4),
+                new THREE.Face3(0,1,4),
+                new THREE.Face3(1,2,4),
+                new THREE.Face3(2,3,4));
+
+forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
+
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh(forma,material);
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
