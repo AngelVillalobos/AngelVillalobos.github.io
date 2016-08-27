656afb9cb0a16a570b0aff8a33b26e106b3af388
AngelVillalobos <langelvilla22@gmail.com>
2016-08-27 15:47:28 -0500

    Update EjemploGeometry.js

--- rv/EjemploGeometry.js
+++ rv/EjemploGeometry.js
@@ -6,7 +6,12 @@ forma.vertices.push(new THREE.Vector3(1,0,1),
                     new THREE.Vector3(-1,0,1),
                     new THREE.Vector3(0,1,0));
 
-forma.faces.push(new THREE.Face3(3,2,1));
+forma.faces.push(new THREE.Face3(3,2,1),
+                new THREE.Face3(3,1,0),
+                new THREE.Face3(3,0,4),
+                new THREE.Face3(0,1,4),
+                new THREE.Face3(1,2,4),
+                new THREE.Face3(2,3,4));
 
 forma.computeBoundingSphere(); //¿¿¿Para qué funciona ésta esfera???
 
