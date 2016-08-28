b6d6dd3efc16451cf643f862f906bad35deb4e98
AngelVillalobos <langelvilla22@gmail.com>
2016-08-28 12:12:31 -0500

    Update Ejercicio4.js

--- rv/Ejercicio4.js
+++ rv/Ejercicio4.js
@@ -16,7 +16,7 @@ var malla = new THREE.Mesh(forma);
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=100;
+camara.position.z=300;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
