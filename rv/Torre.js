515948536f69aafd25e33a972d5164e98c307b20
AngelVillalobos <langelvilla22@gmail.com>
2016-08-30 15:27:13 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -1,5 +1,5 @@
 var BaseForma = new THREE.CylinderGeometry(0.5,0.5,0.2);
-var TorreForma = new THREE.CylinderGeometry(0.4,0.3,0.5);
+var TorreForma = new THREE.CylinderGeometry(0.3,0.4,1);
 
 TorreForma.translate(0,0.2,0);
 
