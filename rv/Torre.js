10de07d0553f56e2f6c94c1c1c018878b3976f9f
AngelVillalobos <langelvilla22@gmail.com>
2016-08-30 15:37:55 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -1,15 +1,19 @@
-var BaseForma = new THREE.CylinderGeometry(0.5,0.5,0.2);
+var BaseForma = new THREE.CylinderGeometry(0.6,0.6,0.2);
 var TorreForma = new THREE.CylinderGeometry(0.3,0.4,1);
+var CoronaForma = new THREE.CylinderGeometry(0.5,0.5,0.4);
 
+CoronaForma.translate(0,1.3,0);
 TorreForma.translate(0,0.6,0);
 
 var BaseMalla = new THREE.Mesh(BaseForma);
 var TorreMalla = new THREE.Mesh(TorreForma);
+var CoronaMalla = new THREE.Mesh(CoronaForma);
 
 var TorrefForma = new THREE.Geometry();
 
 TorrefForma.merge(BaseMalla.geometry,BaseMalla.matrix);
 TorrefForma.merge(TorreMalla.geometry,TorreMalla.matrix);
+TorrefForma.merge(CoronaMalla.geometry,CoronaMalla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var TorrefMalla = new THREE.Mesh(TorrefForma,material);
