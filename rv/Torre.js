caf382eb8b770f91c13abb4f10c9da838a6a3467
AngelVillalobos <langelvilla22@gmail.com>
2016-08-31 16:40:05 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -34,3 +34,4 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
+//
