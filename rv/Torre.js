e2592b3f7e1dcdb5b76aa27079561e5987ac467a
AngelVillalobos <langelvilla22@gmail.com>
2016-08-31 16:37:38 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -4,7 +4,7 @@ var CoronaForma = new THREE.CylinderGeometry(0.5,0.5,0.4,64,64);
 var Base2Forma = new THREE.TorusGeometry(0.4,0.1,30,200);
 
 CoronaForma.translate(0,1.3,0);
-TorreForma.translate(0,0.55,0);
+TorreForma.translate(0,0.6,0);
 Base2Forma.translate(0,0,-0.1);
 
 Base2Forma.rotateX(Math.PI/2);
@@ -27,7 +27,7 @@ var TorrefMalla = new THREE.Mesh(TorrefForma,material);
 var escena = new THREE.Scene();
 escena.add(TorrefMalla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=3;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
