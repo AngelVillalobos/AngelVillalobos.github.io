c81b41cf54d0b24b12cf3e7ffc0e313407e5504b
AngelVillalobos <langelvilla22@gmail.com>
2016-08-31 16:42:45 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -2,17 +2,21 @@ var BaseForma = new THREE.CylinderGeometry(0.6,0.6,0.2,64,64);
 var TorreForma = new THREE.CylinderGeometry(0.3,0.4,1,64,64);
 var CoronaForma = new THREE.CylinderGeometry(0.5,0.5,0.4,64,64);
 var Base2Forma = new THREE.TorusGeometry(0.4,0.1,30,200);
+var Base3Forma = new THREE.TorusGeometry(0.3,0.1,30,200);
 
 CoronaForma.translate(0,1.3,0);
 TorreForma.translate(0,0.6,0);
 Base2Forma.translate(0,0,-0.1);
+Base3Forma.translate(0,0,0.4);
 
+Base3Forma.rotateX(Math.PI/2);
 Base2Forma.rotateX(Math.PI/2);
 
 var BaseMalla = new THREE.Mesh(BaseForma);
 var TorreMalla = new THREE.Mesh(TorreForma);
 var CoronaMalla = new THREE.Mesh(CoronaForma);
 var Base2Malla = new THREE.Mesh(Base2Forma);
+var Base3Malla = new THREE.Mesh(Base3Forma);
 
 var TorrefForma = new THREE.Geometry();
 
@@ -20,6 +24,7 @@ TorrefForma.merge(BaseMalla.geometry,BaseMalla.matrix);
 TorrefForma.merge(TorreMalla.geometry,TorreMalla.matrix);
 TorrefForma.merge(CoronaMalla.geometry,CoronaMalla.matrix);
 TorrefForma.merge(Base2Malla.geometry,Base2Malla.matrix);
+TorrefForma.merge(Base3Malla.geometry,Base3Malla.matrix);
 
 TorrefForma.translate(0,-0.6,0);
 
