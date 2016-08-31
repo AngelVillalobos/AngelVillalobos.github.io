2e052b94bd2f4dbcf5c40709195a9ffc07c82751
AngelVillalobos <langelvilla22@gmail.com>
2016-08-31 16:43:30 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -7,7 +7,7 @@ var Base3Forma = new THREE.TorusGeometry(0.3,0.1,30,200);
 CoronaForma.translate(0,1.3,0);
 TorreForma.translate(0,0.6,0);
 Base2Forma.translate(0,0,-0.1);
-Base3Forma.translate(0,0,0.5);
+Base3Forma.translate(0,0,-0.6);
 
 Base3Forma.rotateX(Math.PI/2);
 Base2Forma.rotateX(Math.PI/2);
