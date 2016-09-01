dbae97b7034cad0cec00d8d0c1b0a556ed17ce1a
AngelVillalobos <langelvilla22@gmail.com>
2016-09-01 15:00:15 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -3,15 +3,13 @@ var TorreForma = new THREE.CylinderGeometry(0.3,0.4,1,64,64);
 var CoronaForma = new THREE.CylinderGeometry(0.5,0.5,0.2,64,64);
 var Base2Forma = new THREE.TorusGeometry(0.4,0.1,30,200);
 var Base3Forma = new THREE.TorusGeometry(0.3,0.1,30,200);
-var Corona1Forma = new THREE.RingGeometry(0.3,0.5,30,30,0,0.6);
-
-var Corona1FormaE = new THREE.ExtrudeGeometry(Corona1Forma,{amount:10});
+var Corona1Forma = new THREE.RingGeometry(0.3,2,30,30,0,0.6);
 
 CoronaForma.translate(0,1.3,0);
 TorreForma.translate(0,0.6,0);
 Base2Forma.translate(0,0,-0.1);
 Base3Forma.translate(0,0,-1.1);
-Corona1FormaE.translate(0,2,0);
+Corona1Forma.translate(0,2,0);
 
 Base3Forma.rotateX(Math.PI/2);
 Base2Forma.rotateX(Math.PI/2);
@@ -21,7 +19,7 @@ var TorreMalla = new THREE.Mesh(TorreForma);
 var CoronaMalla = new THREE.Mesh(CoronaForma);
 var Base2Malla = new THREE.Mesh(Base2Forma);
 var Base3Malla = new THREE.Mesh(Base3Forma);
-var Corona1MallaE = new THREE.Mesh(Corona1FormaE);
+var Corona1Malla = new THREE.Mesh(Corona1Forma);
 
 var TorrefForma = new THREE.Geometry();
 
@@ -30,7 +28,7 @@ TorrefForma.merge(TorreMalla.geometry,TorreMalla.matrix);
 TorrefForma.merge(CoronaMalla.geometry,CoronaMalla.matrix);
 TorrefForma.merge(Base2Malla.geometry,Base2Malla.matrix);
 TorrefForma.merge(Base3Malla.geometry,Base3Malla.matrix);
-TorrefForma.merge(Corona1MallaE.geometry,Corona1MallaE.matrix);
+TorrefForma.merge(Corona1Malla.geometry,Corona1Malla.matrix);
 
 TorrefForma.translate(0,-0.6,0);
 
