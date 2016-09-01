1a30485a4d840e33c5de3cc88921ba28f1549817
AngelVillalobos <langelvilla22@gmail.com>
2016-09-01 15:06:06 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -14,7 +14,7 @@ Corona1Forma.translate(0,2,0);
 Base3Forma.rotateX(Math.PI/2);
 Base2Forma.rotateX(Math.PI/2);
 Corona1Forma.rotateX(Math.PI/2);
-//
+
 var BaseMalla = new THREE.Mesh(BaseForma);
 var TorreMalla = new THREE.Mesh(TorreForma);
 var CoronaMalla = new THREE.Mesh(CoronaForma);
