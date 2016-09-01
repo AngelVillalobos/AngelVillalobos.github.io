2e48d8eab2c3dc7d9ff9f017776f215aad73bfd6
AngelVillalobos <langelvilla22@gmail.com>
2016-09-01 15:29:35 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -41,7 +41,7 @@ var TorrefMalla = new THREE.Mesh(TorrefForma,material);
 var escena = new THREE.Scene();
 escena.add(TorrefMalla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=15;
+camara.position.z=8;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
