1fcb5b273fc232edc704ae454a0f613ef1ae471e
AngelVillalobos <langelvilla22@gmail.com>
2016-09-02 12:19:06 -0500

    Update Arco.js

--- Arco.js
+++ Arco.js
@@ -1,8 +1,10 @@
 
 var arco = new THREE.Shape();
 arco.moveTo(0,0);
-arco.lineTo(0,0.2);
-arco.absarc(0,0,0.5,0,Math.PI/2,false);
+arco.lineTo(0.09,0.49);
+arco.absarc(0,0,0.5,0,Math.PI*(4/9),false);
+arco.lineTo(0.34,0.06);
+arco.absarc(0,0,0.35,0,Math.PI*(4/9),true);
 
 var forma = new THREE.ShapeGeometry(arco);
 var malla = new THREE.Mesh(forma);
