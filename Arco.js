8f99f281a2961e991572f4f90322957286257e72
AngelVillalobos <langelvilla22@gmail.com>
2016-09-02 13:24:58 -0500

    Update Arco.js

--- Arco.js
+++ Arco.js
@@ -21,8 +21,8 @@ var forma4 = new THREE.ShapeGeometry(arco4);
 var malla4 = new THREE.Mesh(forma4);
 
 var forma1 = new THREE.ExtrudeGeometry(arco1,{amount:0.05});
-var material1 = new THREE.MeshNormalMaterial();
-var malla1 = new THREE.Mesh(forma1,material1);
+//var material1 = new THREE.MeshNormalMaterial();
+var malla1 = new THREE.Mesh(forma1);
 
 malla1.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
