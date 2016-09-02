e557d58387d2d05ac96abc74edbd7c3c794c0f05
AngelVillalobos <langelvilla22@gmail.com>
2016-09-02 14:03:10 -0500

    Update Arco.js

--- Arco.js
+++ Arco.js
@@ -14,16 +14,28 @@ arco4.absarc(0,0,0.35,Math.PI*(35/18),Math.PI*(14/9),true);
 
 //var forma1 = new THREE.ShapeGeometry(arco1);
 //var malla1 = new THREE.Mesh(forma1);
-var forma2 = new THREE.ShapeGeometry(arco2);
-var malla2 = new THREE.Mesh(forma2);
-var forma3 = new THREE.ShapeGeometry(arco3);
-var malla3 = new THREE.Mesh(forma3);
-var forma4 = new THREE.ShapeGeometry(arco4);
-var malla4 = new THREE.Mesh(forma4);
+//var forma2 = new THREE.ShapeGeometry(arco2);
+//var malla2 = new THREE.Mesh(forma2);
+//var forma3 = new THREE.ShapeGeometry(arco3);
+//var malla3 = new THREE.Mesh(forma3);
+//var forma4 = new THREE.ShapeGeometry(arco4);
+//var malla4 = new THREE.Mesh(forma4);
 
 var forma1 = new THREE.ExtrudeGeometry(arco1,{steps:500,amount:0.01,bevelEnabled:true,bevelSegments:500,bevelSize:0.7,bevelThickness:0.7});
 var material1 = new THREE.MeshNormalMaterial();
 var malla1 = new THREE.Mesh(forma1,material1);
+
+var forma2 = new THREE.ExtrudeGeometry(arco2,{steps:500,amount:0.01,bevelEnabled:true,bevelSegments:500,bevelSize:0.7,bevelThickness:0.7});
+var material2 = new THREE.MeshNormalMaterial();
+var malla2 = new THREE.Mesh(forma2,material2);
+
+var forma3 = new THREE.ExtrudeGeometry(arco3,{steps:500,amount:0.01,bevelEnabled:true,bevelSegments:500,bevelSize:0.7,bevelThickness:0.7});
+var material3 = new THREE.MeshNormalMaterial();
+var malla3 = new THREE.Mesh(forma3,material3);
+
+var forma1 = new THREE.ExtrudeGeometry(arco4,{steps:500,amount:0.01,bevelEnabled:true,bevelSegments:500,bevelSize:0.7,bevelThickness:0.7});
+var material4 = new THREE.MeshNormalMaterial();
+var malla4 = new THREE.Mesh(forma4,material4);
 
 malla1.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
