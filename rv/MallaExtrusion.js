c8d986298a7ddbdf95b05a1675c3edb5bb6c7db3
AngelVillalobos <langelvilla22@gmail.com>
2016-09-02 13:39:13 -0500

    Update MallaExtrusion.js

--- rv/MallaExtrusion.js
+++ rv/MallaExtrusion.js
@@ -6,7 +6,7 @@ figura.lineTo(10,40);
 figura.lineTo(40,40);
 figura.lineTo(10,10);
 
-var forma = new THREE.ExtrudeGeometry(figura,{amount:10});
+var forma = new THREE.ExtrudeGeometry(figura,{amount:10,curveSegments:100});
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh(forma,material);
