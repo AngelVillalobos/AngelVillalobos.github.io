bef6851c65a6a8d2b1a4b4e4a44c7cdd66733452
AngelVillalobos <langelvilla22@gmail.com>
2016-09-02 12:29:18 -0500

    Update Arco.js

--- Arco.js
+++ Arco.js
@@ -17,4 +17,4 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
-
+//
