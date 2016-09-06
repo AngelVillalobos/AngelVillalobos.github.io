4c0fdd03cb914ccb8f185b47e21454644ca0d10d
AngelVillalobos <langelvilla22@gmail.com>
2016-09-06 15:30:17 -0500

    Update Ortografica.js

--- rv/Ortografica.js
+++ rv/Ortografica.js
@@ -12,13 +12,12 @@ camara.position.z=100;
 var cubo=new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math.PI/4);
 
-var esfera1=new THREE.Mesh(new THREE.SphereGeometry(45),new THREE.MeshNormalMaterial());
+var esfera1=new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial());
 esfera1.position.x=50;
 
 var esfera2=new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial());
 esfera2.position.x=-50;
-esfera2.position.z=-100;
-esfera2.position.y=100;
+esfera2.position.z=300;
 
 var escena = new THREE.Scene();
 escena.add(cubo);
