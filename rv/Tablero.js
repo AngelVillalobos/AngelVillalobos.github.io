5d61c73015d5d7f11674423ec4309f4c52636730
AngelVillalobos <langelvilla22@gmail.com>
2016-09-06 20:16:55 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -8,14 +8,14 @@ var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,
 camara.position.z=15;
 
 var Base=new THREE.Mesh(new THREE.BoxGeometry(10,10,0.3),new THREE.MeshBasicMaterial({color:0x412a09}));
-var A1=new THREE.Mesh(new THREE.BoxGeometry(1.25,1.25,0.05),new THREE.MeshBasicMaterial({color:0x000000}));
+var A1=new THREE.Mesh(new THREE.BoxGeometry(1.25,1.25,0.05),new THREE.MeshBasicMaterial({color:0xffffff}));
 
 Base.rotateX(-Math.PI/3);
 A1.rotateX(-Math.PI/3);
 
 A1.position.y=0.2;
 A1.position.x=-5;
-A1.position.z=-5;
+A1.position.z=5;
 
 var escena = new THREE.Scene();
 escena.add(Base);
