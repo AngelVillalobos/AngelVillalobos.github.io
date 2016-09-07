cc77fa25b95e3f92e88ff866737cd05446b48f36
AngelVillalobos <langelvilla22@gmail.com>
2016-09-06 20:27:28 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -10,8 +10,8 @@ camara.position.z=15;
 var Base=new THREE.Mesh(new THREE.BoxGeometry(10,10,0.3),new THREE.MeshBasicMaterial({color:0x412a09}));
 var CA1=new THREE.Mesh(new THREE.BoxGeometry(0.625,0.625,0.05),new THREE.MeshBasicMaterial({color:0xffffff}));
 
-Base.rotateX(-Math.PI/3);
-CA1.rotateX(-Math.PI/3);
+Base.rotateX(-Math.PI/2);
+CA1.rotateX(-Math.PI/2);
 
 CA1.position.y=0.2;
 CA1.position.x=-4.375;
