490b62ad01a409903f9456eff6de48dd252c192a
AngelVillalobos <langelvilla22@gmail.com>
2016-09-08 15:53:26 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -28,7 +28,7 @@ for (var i=0;i<33; i ++)
       escena.add(CasillaB[i]);
   }
 
-escena.add(Tablero);
+//escena.add(Tablero);
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
