986e43ba257fc73a4865a84572a8238d373359f0
AngelVillalobos <langelvilla22@gmail.com>
2016-09-08 14:56:17 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -1,11 +1,11 @@
 var campoVision=45;
 var relacionAspecto=window.innerWidth/window.innerHeight;
-var planoCercano=200;
+var planoCercano=100;
 var planoLejano=1000;
 
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 
-camara.position.z=15;
+camara.position.z=150;
 
 var BaseMalla=new THREE.Mesh(new THREE.BoxGeometry(100,100,0.3),new THREE.MeshBasicMaterial({color:0x412a09}));
 var Casilla1=new THREE.Mesh(new THREE.BoxGeometry(10,10,0.03),new THREE.MeshBasicMaterial({color:0xffffff}));
