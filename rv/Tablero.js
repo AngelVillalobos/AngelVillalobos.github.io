4d5de83b005f3bc1224e8921135b84265c4ab375
AngelVillalobos <langelvilla22@gmail.com>
2016-09-08 19:02:03 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -22,12 +22,12 @@ var escena = new THREE.Scene();
 
 var a=1;
 /////////////////////////////////
-for (var i=0;i<5; i ++)
+for (var i=0;i<8; i ++)
   {
       CasillaB[i]=new THREE.Mesh(FCasillaB,MCasillaB);
       CasillaG[i]=new THREE.Mesh(FCasillaG,MCasillaG);
   }
-for (var i=0;i<9; i ++)
+for (var i=0;i<8; i ++)
   {
       //for (var j=0;j<32; j ++)
         //{
