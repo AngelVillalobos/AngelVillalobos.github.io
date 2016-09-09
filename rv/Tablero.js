23506fd4a51911ec3f2c29c4de1154be7ad8991b
AngelVillalobos <langelvilla22@gmail.com>
2016-09-08 19:51:13 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -35,13 +35,15 @@ for (var j=0;j<8; j ++)
     if(j%2==0)
       {
         CasillaG[j].position.set((j*10)-50,(i*10)+10,1);
+        escena.add(CasillaG[j]);
       }
     else
       {
         CasillaB[j].position.set((j*10)-50,(i*10)+10,1);
+        escena.add(CasillaB[j]);
       }
-    escena.add(CasillaB[j]);
-    escena.add(CasillaG[j]);
+    
+    
   }
 }
 
