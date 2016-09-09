187483de58b5becf98ddfea4749777daf2fd04a0
AngelVillalobos <langelvilla22@gmail.com>
2016-09-08 19:46:18 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -27,59 +27,24 @@ for (var i=0;i<8; i ++)
       CasillaB[i]=new THREE.Mesh(FCasillaB,MCasillaB);
       CasillaG[i]=new THREE.Mesh(FCasillaG,MCasillaG);
   }
-  
+
+for (var i=0;i<8; i ++)
+{
 for (var j=0;j<8; j ++)
   {
     if(j%2==0)
       {
-        CasillaG[j].position.set((j*10)-50,90,1);
+        CasillaG[j].position.set((j*10)-50,(i*10)+10,1);
       }
     else
       {
-        CasillaB[j].position.set((j*10)-50,90,1);
+        CasillaB[j].position.set((j*10)-50,(i*10)+10,1);
       }
     escena.add(CasillaB[j]);
     escena.add(CasillaG[j]);
   }
-for (var j=0;j<8; j ++)
-  {
-    if(j%2==0)
-      {
-        CasillaB[j].position.set((j*10)-50,80,1);
-      }
-    else
-      {
-        CasillaG[j].position.set((j*10)-50,80,1);
-      }
-    escena.add(CasillaB[j]);
-    escena.add(CasillaG[j]);
-  }
-for (var j=0;j<8; j ++)
-  {
-    if(j%2==0)
-      {
-        CasillaG[j].position.set((j*10)-50,70,1);
-      }
-    else
-      {
-        CasillaB[j].position.set((j*10)-50,70,1);
-      }
-    escena.add(CasillaB[j]);
-    escena.add(CasillaG[j]);
-  }
-for (var j=0;j<8; j ++)
-  {
-    if(j%2==0)
-      {
-        CasillaB[j].position.set((j*10)-50,60,1);
-      }
-    else
-      {
-        CasillaG[j].position.set((j*10)-50,60,1);
-      }
-    escena.add(CasillaB[j]);
-    escena.add(CasillaG[j]);
-  }
+}
+
   
 
 //escena.add(Tablero);
