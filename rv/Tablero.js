b71318cb03c95a92474b625d01867af92f8a9d11
AngelVillalobos <langelvilla22@gmail.com>
2016-09-10 10:41:01 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -28,21 +28,18 @@ var c=1;
 
 for (var i=0;i<71; i ++)
   {
-      CasillaB[i]=new THREE.Mesh(FCasillaB);//,MCasillaB);
-      CasillaG[i]=new THREE.Mesh(FCasillaG);//,MCasillaG);
+      CasillaB[i]=new THREE.Mesh(FCasillaB,MCasillaB);
+      CasillaG[i]=new THREE.Mesh(FCasillaG,MCasillaG);
   }
 for (var j=0;j<71; j ++)
   {
     if(j%2==0)
       {
         CasillaG[j].position.set((c*10)-45,(b*10)-(35-(b*0.5)),0.6);
-        
-        //CasillaG[j].rotateX(-Math.PI/6);
-        
-        //escena.add(CasillaG[j]);
+        CasillaG[j].rotateX(-Math.PI/6);
+        escena.add(CasillaG[j]);
         a=a+1;
       }
-      
     else
       {
         CasillaB[j].position.set((c*10)-45,(b*10)-(35-(b*0.5)),0.6);
@@ -50,8 +47,6 @@ for (var j=0;j<71; j ++)
         escena.add(CasillaB[j]);
         a=a+1;
       }
-      Casillas.merge(CasillaG[j].geometry,CasillaG[j].matrix);
-      Casillas.merge(CasillaB[j].geometry,CasillaB[j].matrix);
   if(a==9)
     {
       a=1;
@@ -59,7 +54,6 @@ for (var j=0;j<71; j ++)
       c=0;
       j=j+1;
     }
-    
     c=c+1;
   }
 
@@ -67,7 +61,7 @@ var material = new THREE.MeshBasicMaterial({color:0x6b6b6b});
 var CasillasFinal = new THREE.Mesh(Casillas,material);
 
 Tablero.rotateX(-Math.PI/6);
-escena.add(CasillasFinal);
+escena.add(Tablero);
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
