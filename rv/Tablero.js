a733b885104cc6bf6c3bac7a69301641790ea163
AngelVillalobos <langelvilla22@gmail.com>
2016-09-10 11:12:27 -0500

    Update Tablero.js

--- rv/Tablero.js
+++ rv/Tablero.js
@@ -5,7 +5,7 @@ var planoLejano=1000;
 
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 
-camara.position.z=150;
+//camara.position.z=150;
 //camara.position.y=150;
 camara.position.x=150;
 
