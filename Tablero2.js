a61541a1052e2ad75144859b6fa77f12cc0145c1
AngelVillalobos <langelvilla22@gmail.com>
2016-09-15 15:02:49 -0500

    Update Tablero2.js

--- Tablero2.js
+++ Tablero2.js
@@ -39,7 +39,7 @@ for (var i=0;i<71; i ++)
       CasillaB[i]=new THREE.Mesh(FCasillaB,MCasillaB);
       CasillaB[i].receiveShadow=true;
       CasillaG[i]=new THREE.Mesh(FCasillaG,MCasillaG);
-      CasillaB[i].receiveShadow=true;
+      CasillaG[i].receiveShadow=true;
   }
 for (var j=0;j<71; j ++)
   {
