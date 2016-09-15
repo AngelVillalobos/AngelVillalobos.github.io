b64f79b576db1be257c334825881980bd2fbfbef
AngelVillalobos <langelvilla22@gmail.com>
2016-09-15 14:37:03 -0500

    Update ejemplo-basicMaterial.js

--- rv/ejemplo-basicMaterial.js
+++ rv/ejemplo-basicMaterial.js
@@ -8,9 +8,10 @@ escena.add(malla);
 var camara = new.THREE.PerspectiveCamera();
 camara.position.z=5;
 
-var lienzo = document.getElementById("basicMaterial");
+//var lienzo = document.getElementById("basicMaterial");
 var renderizador = new THREE.WebGLRenderer({canvas: lienzo,antialias: true});
 
 renderizador.setSize( 600, 600 );
+document.body.appendChild(renderizador.domElement);
 renderizador.render( escena, camara );
 
