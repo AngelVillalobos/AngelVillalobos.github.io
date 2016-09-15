8ee12ba6903879a844533ccd5a40740d6e5b3b6c
AngelVillalobos <langelvilla22@gmail.com>
2016-09-15 16:09:25 -0500

    Update Tablero3.js

--- Tablero3.js
+++ Tablero3.js
@@ -4,13 +4,13 @@ camara.left=window.innerWidth/-2;
 camara.right=window.innerWidth/2;
 camara.top=window.innerHeight/2;
 camara.bottom=window.innerHeight/-2;
-camara.near=0.1;
-camara.far=1000;
+camara.near=0.01;
+camara.far=100;
 camara.updateProjectionMatrix();
 //
 
-camara.position.z=3;
-//camara.position.y=-10;
+//camara.position.z=3;
+camara.position.y=5;
 //camara.position.x=50;
 
 
