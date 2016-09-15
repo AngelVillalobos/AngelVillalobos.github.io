37cff11eeaf9c8421c6ecfb321692f03d7b9bcb8
AngelVillalobos <langelvilla22@gmail.com>
2016-09-15 14:46:29 -0500

    Update ejemplo-basicMaterial.js

--- rv/ejemplo-basicMaterial.js
+++ rv/ejemplo-basicMaterial.js
@@ -1,17 +1,19 @@
-var forma = new THREE.SphereGeometry( 1 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-var malla = new THREE.Mesh( forma, material );
+var forma=new THREE.SphereGeometry(1);
+var material=new THREE.MeshBasicMaterial({color:0x0ff00});
+var malla=new THREE.Mesh(forma,material);
 
-var escena = new THREE.Scene();
+var escena=new THREE.Scene();
 escena.add(malla);
 
-var camara = new.THREE.PerspectiveCamera();
+var camara=new.THREE.PerspectiveCamera();
 camara.position.z=5;
 
-//var lienzo = document.getElementById("basicMaterial");
-var renderizador = new THREE.WebGLRenderer({canvas: lienzo,antialias: true});
+var renderizador=new THREE.WebGLRenderer();
 
-renderizador.setSize( 600, 600 );
+//var lienzo = document.getElementById("basicMaterial");
+//var renderizador = new THREE.WebGLRenderer({canvas: lienzo,antialias: true});
+
+renderizador.setSize(600,600);
 document.body.appendChild(renderizador.domElement);
-renderizador.render( escena, camara );
+renderizador.render(escena,camara);
 
