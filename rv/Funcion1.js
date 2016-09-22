9cdf68f4ec52133290f70e9d3ce5534d64526965
AngelVillalobos <langelvilla22@gmail.com>
2016-09-22 15:24:03 -0500

    Update Funcion1.js

--- rv/Funcion1.js
+++ rv/Funcion1.js
@@ -1,14 +1,14 @@
 /*
 -En JS las funciones son objetos de primer orden
 -En JS, al descender semánticamente de Lisp, maneja las funciones como valores
-
+Ejemplo 1
 function init(p)
 {
   var malla=new THREE.Mesh(new THREE.BoxGeometry(p,p,p),new THREE.MeshNormalMaterial());
   escena=new THREE.Scene();
   escena.add(malla);
   camara=new THREE.PerspectiveCamera();
-  var renderizador = new THREE.WebGLRenderer();
+  renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
   document.body.appendChild(renderizador.domElement);
   camara.position.z=5*p;
@@ -20,9 +20,7 @@ var main=function(p)
 var escena,camara,renderizador;
 init(1);
 main(2);
-
-*/
-
+Ejemplo2
 function init(p)
 {
   var malla=new THREE.Mesh(new THREE.BoxGeometry(p,p,p),new THREE.MeshNormalMaterial());
@@ -42,3 +40,24 @@ var main=function(p)
 var escena,camara,renderizador;
 
 main(init);
+Ejemplo3*/
+function init(p)
+{
+  malla=new THREE.Mesh(new THREE.BoxGeometry(p,p,p),new THREE.MeshNormalMaterial());
+  escena=new THREE.Scene();
+  escena.add(malla);
+  camara=new THREE.PerspectiveCamera();
+  renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(700,700);
+  document.body.appendChild(renderizador.domElement);
+  camara.position.z=5*p;
+}
+var main=function(p)
+{
+  requestAnimationFrame(loop);
+  renderizador.render(escena,camara);
+  malla.rotateY(0.01);
+}
+var escena,camara,renderizador,malla;
+init(1);
+loop();
