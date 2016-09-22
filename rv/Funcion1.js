d989eff0353657f1852a939b4121c5b01c92168c
AngelVillalobos <langelvilla22@gmail.com>
2016-09-22 15:54:57 -0500

    Update Funcion1.js

--- rv/Funcion1.js
+++ rv/Funcion1.js
@@ -80,13 +80,7 @@ var loop=function()
   requestAnimationFrame(loop);
   renderizador.render(escena,camara);
   malla.rotateY(0.01);
-  if(Math.abs(malla.position.x)>=2)//||Math.abs(malla.position.y)>=1)
-    step=-step;
-    malla.position.x+=step;
-    malla.position.y+=step;
- // if(Math.abs(malla.position.y)>=2)
-   // step=-step;
-    //malla.position.y+=step;
+  if(Math.abs(malla.position.x)>=2)
 }
 var escena,camara,renderizador,malla,step;
 init(1);
