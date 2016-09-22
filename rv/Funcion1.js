3c014decdb7f72dd555f1c97dbd44a9800a42458
AngelVillalobos <langelvilla22@gmail.com>
2016-09-22 15:16:08 -0500

    Update Funcion1.js

--- rv/Funcion1.js
+++ rv/Funcion1.js
@@ -20,3 +20,25 @@ var main=function(p)
 var escena,camara,renderizador;
 init(1);
 main(2);
+
+*/
+
+function init(p)
+{
+  var malla=new THREE.Mesh(new THREE.BoxGeometry(p,p,p),new THREE.MeshNormalMaterial());
+  escena=new THREE.Scene();
+  escena.add(malla);
+  camara=new THREE.PerspectiveCamera();
+  var renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(700,700);
+  document.body.appendChild(renderizador.domElement);
+  camara.position.z=5*p;
+}
+var main=function(p)
+{
+  p(1);
+  renderizador.render(escena,camara);
+}
+var escena,camara,renderizador;
+
+main(init);
