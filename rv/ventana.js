83a719712eb11746e7d3926607d22a1d579bb5f0
AngelVillalobos <langelvilla22@gmail.com>
2016-09-27 14:56:01 -0500

    Create ventana.js

---
+++ rv/ventana.js
@@ -0,0 +1,33 @@
+var ventana=new Object();
+
+ventana.listener=function()
+{
+  ventana.camara.aspect=window.innerWidth/window.innerHeight;
+  ventana.camara.updateProjectionMatrix();
+  ventana.renderizador.setSize(window.innerWidth,window.innerHeight);
+}
+
+ventana.setup=function()
+{
+  var tipo_evento='resize';
+  var capturarp=false;
+  window.addEventListener(tipo_evento,ventana.listener,capturarp);
+  ventana.escena=new THREE.Scene();
+  ventana.camara=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+  ventana.camara.position.z=5;
+  var renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(window.innerHeight,window.innerHeight);
+  document.body.appendChild(renderizador.domElement);
+  ventana.malla=new THREE.Mesh(new TREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+  centana.escena.add(ventana.malla);
+}
+
+ventana.loop=function()
+{
+  requestAnimationFrame(ventana.loop);
+  ventana.malla.rotateX=0.01;
+  ventana.malla.rotateY=0.01;
+  ventana.renderizador.render(ventana.escena,centana.camara);
+}
+ventana.setup();
+centana.loop();
