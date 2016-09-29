dfb7c1777f627bf481540b0eb09b0ae7c91d3291
AngelVillalobos <langelvilla22@gmail.com>
2016-09-29 16:04:53 -0500

    Update prueba1.js

--- rv/prueba1.js
+++ rv/prueba1.js
@@ -66,7 +66,7 @@ TEXTURATB.setup=function()
   cargador.load("maderaB.jpg",TEXTURATB.retrollamada);
   TEXTURATB.camara=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
   TEXTURATB.camara.position.z=10;
-  TEXTURATB.camara.position.y=10;
+  TEXTURATB.camara.position.y=-10;
   var lienzo=document.getElementById("ejemplo-textura");
   TEXTURATB.renderizador=new THREE.WebGLRenderer({canvas:lienzo,antialiaas:true});
   TEXTURATB.renderizador.setSize(600,600);
