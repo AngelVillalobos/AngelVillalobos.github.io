46a9c7674436d770504d184af315c9538dcbf91a
AngelVillalobos <langelvilla22@gmail.com>
2016-09-29 15:24:43 -0500

    Update etexturas.js

--- etexturas.js
+++ etexturas.js
@@ -1,14 +1,10 @@
 var TEXTURA=new Object();
 
-TEXTURA.retrollamada=function(textura1,textura2)
+TEXTURA.retrollamada=function(textura)
 {
-  var material=new THREE.MeshBasicMaterial({map:textura1});
-  var material1=new THREE.MeshBasicMaterial({map:textura2});
+  var material=new THREE.MeshBasicMaterial({map:textura});
   TEXTURA.malla=new THREE.Mesh(new THREE.SphereGeometry(1,32,32),material);
-  TEXTURA.malla1=new THREE.Mesh(new THREE.SphereGeometry(1,32,32),material1);
-  TEXTURA.malla1.position.set(2,2,0)
   TEXTURA.escena.add(TEXTURA.malla);
-  TEXTURA.escena.add(TEXTURA.malla1);
 }
 
 
@@ -16,7 +12,7 @@ TEXTURA.setup=function()
 {
   TEXTURA.escena=new THREE.Scene();
   var cargador=new THREE.TextureLoader();
-  cargador.load("tierra.jpg","maderaB.jpg",TEXTURA.retrollamada);
+  cargador.load("maderaB.jpg",TEXTURA.retrollamada);
   TEXTURA.camara=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
   TEXTURA.camara.position.z=5;
   var lienzo=document.getElementById("ejemplo-textura");
