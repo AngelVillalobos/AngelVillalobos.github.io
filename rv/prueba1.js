180a911dfd882a7a2082404bc66bfdb7f56e8ac1
AngelVillalobos <langelvilla22@gmail.com>
2016-10-01 13:33:13 -0500

    Update prueba1.js

--- rv/prueba1.js
+++ rv/prueba1.js
@@ -6,7 +6,7 @@ TEXTURATN.malla=new Array();
 /////////////DEFINICIÓN DE CAMARA Y RENDERIZADOR///////////////////////////////////
 var campoVision=45;
 var relacionAspecto=window.innerWidth/window.innerHeight;
-var planoCercano=100;
+var planoCercano=1;
 var planoLejano=1000;
 TEXTURATB.camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 TEXTURATB.camara.position.z=5;
