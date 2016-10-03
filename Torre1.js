0d324ebf666c3141458df287e4899665d1872c90
AngelVillalobos <langelvilla22@gmail.com>
2016-10-03 13:22:32 -0500

    Update Torre1.js

--- Torre1.js
+++ Torre1.js
@@ -2,7 +2,7 @@ var BaseForma = new THREE.CylinderGeometry(0.5,0.5,0.15,64,64);
 var Base2Forma = new THREE.TorusGeometry(0.35,0.1,30,200);
 var Base3Forma = new THREE.TorusGeometry(0.2,0.07,30,200);
 var TorreForma = new THREE.CylinderGeometry(0.2,0.4,0.6,64,64);
-var Corona3Forma = new THREE.SphereGeometry(0.1,32,32,6,6.3,6,6.3);
+var Corona3Forma = new THREE.SphereGeometry(0.5,32,32,6,6.3,6,6.3);
 
 Corona3Forma.translate(0,-1.3,0);
 TorreForma.translate(0,0.375,0);
