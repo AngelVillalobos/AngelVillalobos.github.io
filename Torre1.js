6c4ae2cf93d3086f74c7c6d8b9e46960c5f15ac8
AngelVillalobos <langelvilla22@gmail.com>
2016-10-03 12:06:17 -0500

    Update Torre1.js

--- Torre1.js
+++ Torre1.js
@@ -44,7 +44,7 @@ TorrefForma.merge(Corona3Malla.geometry,Corona3Malla.matrix);
 
 
 
-TorrefForma.translate(0,0,0);
+TorrefForma.translate(0,0,-1);
 
 var material = new THREE.MeshNormalMaterial();
 var TorrefMalla = new THREE.Mesh(TorrefForma,material);
