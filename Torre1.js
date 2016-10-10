d5886f9bbb2927af7548eaec7444ee5962f27c50
AngelVillalobos <langelvilla22@gmail.com>
2016-10-10 13:56:00 -0500

    Update Torre1.js

--- Torre1.js
+++ Torre1.js
@@ -2,8 +2,15 @@ var BaseForma = new THREE.CylinderGeometry(0.5,0.5,0.15,64,64);
 var Base2Forma = new THREE.TorusGeometry(0.35,0.1,30,200);
 var Base3Forma = new THREE.TorusGeometry(0.2,0.07,30,200);
 var TorreForma = new THREE.CylinderGeometry(0.2,0.4,0.6,64,64);
-var Corona3Forma = new THREE.SphereGeometry(0.36,32,32,6,6.3,6,6.3);
+var Corona3Forma = new THREE.SphereGeometry(0.2,32,32,6,6.3,6,6.3);
+var Puntos = [];
+for (var i=-4;i<10;i++)
+{
+	Puntos.push(new THREE.Vector2(Math.cos(i*0.2)*0.2+0.1,(i-0.1)*0.05));
+}
+var Corona1Forma=new THREE.LatheGeometry(Puntos);
 
+Corona1Forma.translate(0,-0.5,0);
 Corona3Forma.translate(0,-1,0);
 TorreForma.translate(0,0.375,0);
 Base2Forma.translate(0,0,-0.08);
@@ -18,7 +25,7 @@ var Base2Malla = new THREE.Mesh(Base2Forma);
 var Base3Malla = new THREE.Mesh(Base3Forma);
 var TorreMalla = new THREE.Mesh(TorreForma);
 var Corona3Malla = new THREE.Mesh(Corona3Forma);
-
+var Corona1Malla = new THREE.Mesh(Corona1Forma);
 
 var TorrefForma = new THREE.Geometry();
 
@@ -27,6 +34,7 @@ TorrefForma.merge(Base2Malla.geometry,Base2Malla.matrix);
 TorrefForma.merge(Base3Malla.geometry,Base3Malla.matrix);
 TorrefForma.merge(TorreMalla.geometry,TorreMalla.matrix);
 TorrefForma.merge(Corona3Malla.geometry,Corona3Malla.matrix);
+TorrefForma.merge(Corona1Malla.geometry,Corona1Malla.matrix);
 
 
 
@@ -38,17 +46,11 @@ TorrefMalla.rotateX(Math.PI/12);
 
 
 
-var points = [];
-for ( var i = -4; i < 10; i ++ ) {
-	points.push( new THREE.Vector2( Math.cos( i * 0.2 ) * 0.2 + 0.1, ( i - 0.1 ) * 0.05 ) );
-}
-var geometry = new THREE.LatheGeometry( points );
-var material = new THREE.MeshNormalMaterial();
-var lathe = new THREE.Mesh( geometry, material );
+
+
 //lathe.position.y=15
-var scene = new THREE.Scene();
-scene.add( lathe );
-scene.add(TorrefMalla);
+var escena = new THREE.Scene();
+escena.add(TorrefMalla);
 
 
 var camara = new THREE.PerspectiveCamera();
