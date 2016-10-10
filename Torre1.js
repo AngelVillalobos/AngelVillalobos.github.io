e2788eb4be309f569a948f26b3a1007cc87c8259
AngelVillalobos <langelvilla22@gmail.com>
2016-10-10 12:42:37 -0500

    Update Torre1.js

--- Torre1.js
+++ Torre1.js
@@ -44,7 +44,7 @@ for ( var i = 0; i < 10; i ++ ) {
 	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
 }
 var geometry = new THREE.LatheGeometry( points );
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var material = new THREE.MeshNormalMaterial();
 var lathe = new THREE.Mesh( geometry, material );
 var scene = new THREE.Scene();
 scene.add( lathe );
@@ -52,7 +52,7 @@ scene.add( lathe );
 
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=20;
+camara.position.z=50;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
