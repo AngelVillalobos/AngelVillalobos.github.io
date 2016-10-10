9afacfd3634ba9ebcfbc2c78885d0aebb359a811
AngelVillalobos <langelvilla22@gmail.com>
2016-10-10 13:34:39 -0500

    Update Torre1.js

--- Torre1.js
+++ Torre1.js
@@ -40,7 +40,7 @@ var TorrefMalla = new THREE.Mesh(TorrefForma,material);
 
 var points = [];
 for ( var i = -4; i < 10; i ++ ) {
-	points.push( new THREE.Vector2( Math.cos( i * 0.2 ) * 0.1 + 0.05, ( i - 0.05 ) * 0.02 ) );
+	points.push( new THREE.Vector2( Math.cos( i * 0.2 ) * 0.5 + 0.25, ( i - 0.25 ) * 0.1 ) );
 }
 var geometry = new THREE.LatheGeometry( points );
 var material = new THREE.MeshNormalMaterial();
