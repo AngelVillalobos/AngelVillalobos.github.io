be568a767c3dd28ca5469aaef1edb313136eee8c
AngelVillalobos <langelvilla22@gmail.com>
2016-10-10 12:41:30 -0500

    Update Torre1.js

--- Torre1.js
+++ Torre1.js
@@ -52,7 +52,7 @@ scene.add( lathe );
 
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=3;
+camara.position.z=20;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
