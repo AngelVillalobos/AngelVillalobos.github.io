683d336ec9876150a5c479ce4dadcf6eada40bcf
AngelVillalobos <langelvilla22@gmail.com>
2016-10-13 16:43:47 -0500

    Update prueba2.js

--- rv/prueba2.js
+++ rv/prueba2.js
@@ -5,6 +5,8 @@ AJEDREZ.PeonesBlancos=new Array();
 AJEDREZ.PeonesNegros=new Array();
 AJEDREZ.CasillasNegras=new Array();
 AJEDREZ.CasillasBlancas=new Array();
+AJEDREZ.AlfilesBlancos=new Array();
+AJEDREZ.AlfilesNegros=new Array();
 
 AJEDREZ.CRL=function()
 {
@@ -51,6 +53,47 @@ AJEDREZ.TableroGeometry=function()
   this.merge(MTablero.geometry,MTablero.matrix);
 }
 AJEDREZ.TableroGeometry.prototype=new THREE.Geometry();
+////////////////////////////////////////////////////////////////////
+
+///////////////CONSTRUCTOR DEL ALFIL////////////////////////////////
+AJEDREZ.AlfilGeometry=function()
+{
+  THREE.Geometry.call(this);
+  var BaseAlForma = new THREE.CylinderGeometry(0.5,0.5,0.15,64,64);
+  var Base2AlForma = new THREE.TorusGeometry(0.35,0.1,30,200);
+  var Base3AlForma = new THREE.TorusGeometry(0.2,0.07,30,200);
+  var AlfilForma = new THREE.CylinderGeometry(0.2,0.4,1.2,64,64);
+  var Corona3AlForma = new THREE.SphereGeometry(0.08,32,32,6,6.3,6,6.3);
+  var Puntos = [];
+  for (var i=-4;i<10;i++)
+  {
+  	Puntos.push(new THREE.Vector2(Math.cos(i*0.2)*0.2+0.1,(i-0.1)*0.05));
+  }
+  var Corona1AlForma=new THREE.LatheGeometry(Puntos);
+  Corona1AlForma.translate(0,1.52,0);
+  Corona3AlForma.translate(0,-2,0);
+  AlfilForma.translate(0,0.675,0);
+  Base2AlForma.translate(0,0,-0.08);
+  Base3AlForma.translate(0,0,-1.25);
+  Corona1AlForma.rotateY(Math.PI);
+  Corona3AlForma.rotateX(Math.PI);
+  Base3AlForma.rotateX(Math.PI/2);
+  Base2AlForma.rotateX(Math.PI/2);
+  var BaseAlMalla = new THREE.Mesh(BaseAlForma);
+  var Base2AlMalla = new THREE.Mesh(Base2AlForma);
+  var Base3AlMalla = new THREE.Mesh(Base3AlForma);
+  var AlfilMalla = new THREE.Mesh(AlfilForma);
+  var Corona3AlMalla = new THREE.Mesh(Corona3AlForma);
+  var Corona1AlMalla = new THREE.Mesh(Corona1AlForma);
+  var AlfilfForma = new THREE.Geometry();
+  this.merge(BaseAlMalla.geometry,BaseAlMalla.matrix);
+  this.merge(Base2AlMalla.geometry,Base2AlMalla.matrix);
+  this.merge(Base3AlMalla.geometry,Base3AlMalla.matrix);
+  this.merge(AlfilMalla.geometry,AlfilMalla.matrix);
+  this.merge(Corona3AlMalla.geometry,Corona3AlMalla.matrix);
+  this.merge(Corona1AlMalla.geometry,Corona1AlMalla.matrix);
+}
+AJEDREZ.AlfilGeometry.prototype=new THREE.Geometry();
 ////////////////////////////////////////////////////////////////////
 
 /////////////CONSTRUCTOR DE PEON///////////////////////////////////
@@ -127,6 +170,23 @@ AJEDREZ.TorreGeometry=function()
   this.merge(Corona3Malla.geometry,Corona3Malla.matrix);
 }
 AJEDREZ.TorreGeometry.prototype=new THREE.Geometry();
+////////////////////////////////////////////////////////////////////
+
+/////////////////CREANDO ALFIL BLANCO///////////////////////////////
+AJEDREZ.RetrollamadaAlfilBlanco=function(textura)
+{
+  var materialAlfilBlanco=new THREE.MeshLambertMaterial({map:textura});
+  for (var i=1;i<3;i++)
+  {
+    AJEDREZ.AlfilesBlancos[i]=new THREE.Mesh(new AJEDREZ.AlfilGeometry(),materialAlfilBlanco);
+    AJEDREZ.AlfilesBlancos[i].rotateX(Math.PI/2);
+    AJEDREZ.AlfilesBlancos[i].scale.set(7,7,8);
+    AJEDREZ.AlfilesBlancos[i].castShadow=true;
+    AJEDREZ.escena.add(AJEDREZ.AlfilesBlancos[i]);
+  }
+  AJEDREZ.AlfilesBlancos[1].position.set(-15,-35,1.2);
+  AJEDREZ.AlfilesBlancos[2].position.set(15,-35,1.2);
+}
 ////////////////////////////////////////////////////////////////////
 
 /////////////////CREANDO TORRE BLANCA///////////////////////////////
@@ -295,6 +355,8 @@ AJEDREZ.RetrollamadaTablero=function(textura)
 
 AJEDREZ.setupPiezas=function()
 {
+  AJEDREZ.cargadorAlfilBlanco=new THREE.TextureLoader(); 
+  AJEDREZ.cargadorAlfilBlanco.load("maderaB.jpg",AJEDREZ.RetrollamadaAlfilBlanco);  
   AJEDREZ.cargadorTorreBlanca=new THREE.TextureLoader(); 
   AJEDREZ.cargadorTorreBlanca.load("maderaB.jpg",AJEDREZ.RetrollamadaTorreBlanca);  
   AJEDREZ.cargadorPeonBlanco=new THREE.TextureLoader(); 
