a69d3b734b138f20da00989b0015f3215e99981e
AngelVillalobos <langelvilla22@gmail.com>
2016-10-20 14:58:02 -0500

    Update Animacion1.js

--- rv/Animacion1.js
+++ rv/Animacion1.js
@@ -4,7 +4,7 @@ function Pieza()
   this.piernaizq=new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
   this.piernader=new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
   var cuerpo=new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
-  this.add(piernaezq,piernader,cuerpo);
+  this.add(piernaizq,piernader,cuerpo);
   this.piernaizq.position.z=-2;
   this.piernaizq.position.y=-2.5;
   this.piernader.position.z=2;
