f4eb41e59dc5b409f836c804d437d09f259745a3
AngelVillalobos <langelvilla22@gmail.com>
2016-10-21 12:10:24 -0500

    Create BlenderLoader.js

---
+++ BlenderLoader.js
@@ -0,0 +1,17 @@
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=4;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
+var MODELO=new THREE.JSONLoader();
+MODELO.load("Rey1.js",funcion1);
+function funcion1(geometry,materials)
+{
+  material=new THREE.MeshBasicMaterial();
+  Modelo=new THREE.Mesh(geometry,material);
+  var escena = new THREE.Scene();
+  escena.add(Modelo);
+}
+
+
