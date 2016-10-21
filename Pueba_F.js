72a2203d35f54bf48d8f1f207cd5d3168325c522
AngelVillalobos <langelvilla22@gmail.com>
2016-10-21 14:18:17 -0500

    Create Pueba_F.js

---
+++ Pueba_F.js
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Tablero</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <style>
+      body{text-align:center;}
+    </style>
+  </head>
+  <body>
+  <script src="./Prueba_1.js"></script>
+  <script src="./Prueba_2.js"></script>
+  <script src="http://mrdoob.github.com/three.js/build/three.min.js"></script>
+  </body>
+</html>
