d6e975d8e2224b557d329430328a9b8770f5a8b8
AngelVillalobos <langelvilla22@gmail.com>
2016-10-24 17:32:25 -0500

    Update BlenderLoader.js

--- BlenderLoader.js
+++ BlenderLoader.js
@@ -6,7 +6,16 @@
   init();
   animate();
 
+  var MaterialCuerpo=new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture('TexturaCuerpoRey.png')});
+  //var MaterialCabeza=new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture('TexturaCabezaRey.png')});
 
+  Cargador=new THREE.JSONLoader();
+  Cargador.load('CuerpoRey.js',function(geometry){
+  Cuerpo=new THREE.Mesh(geometry,MaterialCuerpo);
+  Cuerpo.scale.set(5,5,5);
+  //Cuerpo.rotateY(Math.PI/6);
+  Cuerpo.position.set(0,0,0);
+  });
 
 function init()
 {
@@ -15,22 +24,10 @@ function init()
   luzPuntual.position.y=-50;
   luzPuntual.position.z=50;
   escena.add(luzPuntual);
-
-  var Material=new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture('TexturaCuerpoRey.png')});
-
-  MODELO=new THREE.JSONLoader();
-  MODELO.load('CuerpoRey.js',function(geometry){
-  Modelo=new THREE.Mesh(geometry,Material);
-  //escena.add(BaseAlMalla);
-  Modelo.scale.set(5,5,5);
-  Modelo.rotateY(Math.PI/6);
-  Modelo.position.set(0,0,0);
-  escena.add(Modelo);
-  
-  
-});
   renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
   document.body.appendChild(renderizador.domElement);
+  
+  escena.add(Cuerpo);
 }  
 function animate()
 {
