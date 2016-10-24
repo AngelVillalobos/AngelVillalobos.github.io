1187d35f7758b2fae70ed7c29bd7e00b7cfb5f50
AngelVillalobos <langelvilla22@gmail.com>
2016-10-24 12:58:41 -0500

    Update BlenderLoader.js

--- BlenderLoader.js
+++ BlenderLoader.js
@@ -1,15 +1,13 @@
 
 
 var MODELO=new THREE.JSONLoader();
-MODELO.load("Rey.js",funcion1);
-function funcion1(geometry,materials)
-{
+MODELO.load("Rey.js",funcion1(geometry){
   var escena = new THREE.Scene();
   var camara = new THREE.PerspectiveCamera();
   camara.position.z=4;
   var renderizador = new THREE.WebGLRenderer();
   var BaseAlMalla = new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,0.15,64,64),new THREE.MeshNormalMaterial());
-  material=new THREE.MeshBasicMaterial(materials);
+  material=new THREE.MeshBasicMaterial();
   Modelo=new THREE.Mesh(geometry,material);
   //escena.add(BaseAlMalla);
   escena.add(Modelo);
@@ -18,7 +16,7 @@ function funcion1(geometry,materials)
   renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
   document.body.appendChild(renderizador.domElement);
   renderizador.render(escena,camara);
-}
+});
 //funcion1();
 
 
