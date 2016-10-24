9b55b5dd8cc6b933b98e17e1fd628a7e0c8d1260
AngelVillalobos <langelvilla22@gmail.com>
2016-10-24 13:32:37 -0500

    Update BlenderLoader.js

--- BlenderLoader.js
+++ BlenderLoader.js
@@ -1,24 +1,41 @@
   var escena = new THREE.Scene();
   var camara = new THREE.PerspectiveCamera();
   var renderizador = new THREE.WebGLRenderer();
+  var luzPuntual=new THREE.PointLight(0xFFFFFF);
 
-var MODELO=new THREE.JSONLoader();
-MODELO.load('cubo.js',function(geometry){
-  
-  camara.position.z=4;
-  
-  var BaseAlMalla = new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,0.15,64,64),new THREE.MeshNormalMaterial());
-  material=new THREE.MeshBasicMaterial();
-  Modelo=new THREE.Mesh(geometry,material);
+  init();
+  animate();
+
+function init()
+{
+  camara.position.z=40;
+  luzPuntual.position.x=150;
+  luzPuntual.position.y=-150;
+  luzPuntual.position.z=150;
+  escena.add(luzPuntual);
+
+  var Material=new THREE.MeshLambertMaterial({map:THREE.ImageUtils.loadTexture('Textura.png')});
+
+  MODELO=new THREE.JSONLoader();
+  MODELO.load('Cube.js',function(geometry){
+  //var BaseAlMalla = new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,0.15,64,64),new THREE.MeshNormalMaterial());
+  //material=new THREE.MeshBasicMaterial();
+  Modelo=new THREE.Mesh(geometry,Material);
   //escena.add(BaseAlMalla);
-  
   Modelo.scale.set(15,15,15);
   Modelo.position.set(0,0,0);
   escena.add(Modelo);
+  
+  
+});
   renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
   document.body.appendChild(renderizador.domElement);
+}  
+function animate()
+{
+  requestAnimationFrame(animate);
   renderizador.render(escena,camara);
-});
+}
 //funcion1();
 
 
