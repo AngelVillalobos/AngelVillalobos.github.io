0b1d941f584239c6be6c78c90ca2db43328dee60
AngelVillalobos <langelvilla22@gmail.com>
2016-10-27 15:23:08 -0500

    Update Agente.js

--- rv/Agente.js
+++ rv/Agente.js
@@ -65,7 +65,7 @@ Pelota.prototype.sense=function(environment)
   var obstaculo1=this.sensor.intersectObjects(environment.children,true);
   this.sensor.set(this.position,new THREE.Vector3(-1,0,0));
   var obstaculo2=this.sensor.intersectObjects(environment.children,true);
-  if((obstaculo1.length>0 && (obstaculo1.distance<=this.radius))||(obstaculo2.length>0 && (obstaculo2.distance<=this.radius)))
+  if((obstaculo1.length<0 && (obstaculo1.distance<=this.radius))||(obstaculo2.length<0 && (obstaculo2.distance<=this.radius)))
     this.colision=1;
   else
     this.colision=0;
@@ -74,7 +74,7 @@ Pelota.prototype.sense=function(environment)
 Pelota.prototype.act=function(environment)
 {
   if(this.colision===1)
-    this.step=this.step;
+    this.step=-this.step;
   this.position.x +=this.step;
 }
 
