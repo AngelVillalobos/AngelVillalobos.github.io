721ae55ad740dfc3c5df2c1654e4701d733ee419
AngelVillalobos <langelvilla22@gmail.com>
2016-11-10 15:26:46 -0600

    Update AgenteRobot1.js

--- rv/AgenteRobot1.js
+++ rv/AgenteRobot1.js
@@ -110,7 +110,7 @@ Robot.prototype.act=function(environment)
 {
   var command=this.actuator.pop();
   if(command===undefined)
-    console.log('Indefined command');
+    console.log('Undefined command');
   else if(command in this.operations)
     this.operations[command](this);
   else
@@ -123,21 +123,21 @@ Robot.prototype.operations.goStraight=function(robot,distance)
 {
   if(distance===undefined)
     distance=0.05;
-    robot.position.x+=distance.Math.cos(robot.rotation.z);
-    robot.position.y+=distance.Math.sin(robot.rotation.z);
+  robot.position.x+=distance.Math.cos(robot.rotation.z);
+  robot.position.y+=distance.Math.sin(robot.rotation.z);
 };
 
 Robot.prototype.operations.rotateCW=function(robot,angle)
 {
   if(angle===undefined)
-  angle=-Math.PI/2;
+    angle=-Math.PI/2;
   robot.rotation.z+=angle;
 };
 
 Robot.prototype.operations.rotateCCW=function(robot,angle)
 {
   if(angle===undefined)
-  angle=Math.PI/2;
+    angle=Math.PI/2;
   robot.rotation.z+=angle;
 };
   
