a731622311cc398f65fe90ce549b04b468607306
AngelVillalobos <langelvilla22@gmail.com>
2016-11-11 16:05:39 -0600

    Update PiezaMove.js

--- rv/PiezaMove.js
+++ rv/PiezaMove.js
@@ -188,7 +188,7 @@ Peon.prototype.sense=function(environment)
 {
   this.sensor.set(this.position,new THREE.Vector3(1,0,0));
   var obstaculo=this.sensor.intersectObjects(environment.children,true);
-  if((obstaculo.length>0 && (obstaculo[0].distance<=5)))
+  if((obstaculo.length>0 && (obstaculo[0].distance<=1)))
     this.sensor.colision=true;
   else
     this.sensor.colision=false;
