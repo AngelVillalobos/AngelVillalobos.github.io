f9a25ed9d0b290a95a7301cf07c48b85d5cacaf6
AngelVillalobos <langelvilla22@gmail.com>
2016-11-11 14:55:19 -0600

    Update PiezaMove.js

--- rv/PiezaMove.js
+++ rv/PiezaMove.js
@@ -189,19 +189,19 @@ function Teclado()
   var avance=0.4;
   if(event.keyCode==39)
   {
-    environment.children[40].position.x+=avance;
+    environment.children[91].position.x+=avance;
   }
   else if(event.keyCode==37)
   {
-    environment.children[69].position.x+=-avance;
+    environment.children[91].position.x+=-avance;
   }
   else if(event.keyCode==38)
   {
-    environment.children[80].position.y+=avance;
+    environment.children[92].position.y+=avance;
   }
   else if(event.keyCode==40)
   {
-    environment.children[99].position.y+=-avance;
+    environment.children[92].position.y+=-avance;
   }
 }
 
