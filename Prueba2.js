281ebdb9e78675136e15f4e57987f58518e96eab
AngelVillalobos <langelvilla22@gmail.com>
2016-11-12 14:24:39 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -236,10 +236,10 @@ function onDocumentMouseDown( event ) {
 
             if ( intersects.length > 0 ) {
                 intersects[ 0 ].object.material.color.setHex( 0x00ff00 );
-                
+                var CA=intersects[ 0 ].matrixWorld.getPosition()
           var x = getOffset(intersects[0]).left;
             var y = getOffset(intersects[0]).top;
-                //console.log(W);                     
+                console.log(CA);                     
             }
         }
 
