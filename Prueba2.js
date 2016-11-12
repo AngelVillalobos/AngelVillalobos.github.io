fc2e17133d7f73a6ef81116c328344fee3066147
AngelVillalobos <langelvilla22@gmail.com>
2016-11-12 12:51:40 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -236,10 +236,10 @@ function onDocumentMouseDown( event ) {
                 intersects[ 0 ].object.material.color.setHex( 0x00ff00 );
               //var pos=intersects[0].position.get();
               //console.log(pos);
-              environment.updateMatrixWorld(true);
-              var position = new THREE.Vector3();
-              position.getPositionFromMatrix( intersects[0].matrixWorld );
-              alert(position.x + ',' + position.y + ',' + position.z);
+              intersects[ 0 ].updateMatrixWorld(true);
+              var vector = new THREE.Vector3();
+              vector.setFromMatrixPosition( intersects[ 0 ].matrixWorld );
+              //alert(position.x + ',' + position.y + ',' + position.z);
               
             }
         }
