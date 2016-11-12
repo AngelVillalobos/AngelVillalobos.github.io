6d51402505e8c5f6dac58cd160bea7f332584f2d
AngelVillalobos <langelvilla22@gmail.com>
2016-11-12 14:52:02 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -234,7 +234,7 @@ function onDocumentMouseDown( event ) {
             if ( intersects.length > 0 ) {
                 intersects[ 0 ].object.material.color.setHex( 0x00ff00 );
                 
-          var x = getOffsetSum(intersects[0]).left;
+          var x = getOffsetRect(intersects[0]).left;
             //var y = findPos(intersects[0]).curtop;
                 console.log(x);                     
             }
@@ -244,15 +244,19 @@ function onDocumentMouseup( event ) {
   intersects[ 0 ].object.material.color.setHex( 0xffffff );
         }
 
-function getOffsetSum(elem) {
-  var top=0, left=0
-  while(elem) {
-    top = top + parseInt(elem.offsetTop)
-    left = left + parseInt(elem.offsetLeft)
-    elem = elem.offsetParent       
-  }
-  return {top: top, left: left}
+function getOffsetRect(elem) {
+    var box = elem.getBoundingClientRect()
+    var body = document.body
+    var docElem = document.documentElement
+    var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop
+    var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft
+    var clientTop = docElem.clientTop || body.clientTop || 0
+    var clientLeft = docElem.clientLeft || body.clientLeft || 0
+    var top  = box.top +  scrollTop - clientTop
+    var left = box.left + scrollLeft - clientLeft
+    return { top: Math.round(top), left: Math.round(left) }
 }
+
 
 
 
