0c1f1e73adb645eed0679448ec0b01e1e8ccc537
AngelVillalobos <langelvilla22@gmail.com>
2016-11-12 14:25:38 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -250,7 +250,7 @@ function onDocumentMouseup( event ) {
 function getOffset( el ) {
     var _x = 0;
     var _y = 0;
-    while( el &amp;&amp; !isNaN( el.offsetLeft ) &amp;&amp; !isNaN( el.offsetTop ) ) {
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
         _x += el.offsetLeft - el.scrollLeft;
         _y += el.offsetTop - el.scrollTop;
         el = el.offsetParent;
