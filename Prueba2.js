5ca7ee533a49c9a7c57e54f46c72794791f4a48e
AngelVillalobos <langelvilla22@gmail.com>
2016-11-13 15:05:00 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -350,14 +350,14 @@ function setup()
   
   var Piezas=new Array();
   Piezas[0]="          ";
-  Piezas[1]="          ";
-  Piezas[2]="          ";
+  Piezas[1]=" pppppppp ";
+  Piezas[2]=" pppppppp ";
   Piezas[3]="          ";
   Piezas[4]="          ";
   Piezas[5]="          ";
   Piezas[6]="          ";
-  Piezas[7]="  pp      ";
-  Piezas[8]="          ";
+  Piezas[7]=" pppppppp ";
+  Piezas[8]=" pppppppp ";
   Piezas[9]="          ";
      
   environment=new Environment();
