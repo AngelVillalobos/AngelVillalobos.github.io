92b5bb019122e6222cb45156dbc28d3c9afdcede
AngelVillalobos <langelvilla22@gmail.com>
2016-11-13 14:24:49 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -123,19 +123,19 @@ Environment.prototype.setMapCasilla=function(map)
       if(map[i][j]==="n")
       {
         Casilla=new CasillaBlanca(10,(i*10)-45,(j*10)-45);
-        //Casilla.receiveShadow=true;
+        Casilla.receiveShadow=true;
         this.add(Casilla);
       }
       else if(map[i][j]==="b")
       {
         Casilla=new CasillaNegra(10,(i*10)-45,(j*10)-45);
-        //Casilla.receiveShadow=true;
+        Casilla.receiveShadow=true;
         this.add(Casilla);
       }
       else if(map[i][j]==="B")
       {
         CBorde=new Borde(10,(i*10)-45,(j*10)-45);
-        //CBorde.receiveShadow=true;
+        CBorde.receiveShadow=true;
         this.add(CBorde);
       }
     }
@@ -185,7 +185,7 @@ Peon.prototype.sense=function(environment)
 {
   this.sensor.set(this.position,new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
   var obstaculo=this.sensor.intersectObjects(environment.children,true);
-  if((obstaculo.length>0 && (obstaculo[0].distance<=1.8)))
+  if((obstaculo.length>0 && (obstaculo[0].distance<=2.1)))
     this.sensor.colision=true;
   else
     this.sensor.colision=false;
