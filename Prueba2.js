b8a2cf0643c85b27406fd3fd6fc8defec6519935
AngelVillalobos <langelvilla22@gmail.com>
2016-11-15 14:53:44 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -274,10 +274,9 @@ function SeleccionD(event)
     x=seleccion[0].point.x;
     y=seleccion[0].point.y;
     
-    console.log(x);
+    //console.log(x);
     
     if((-50<x&&x<50&&40<y&&y<50)||(-50<x&&x<50&&-50<y&&y<-40)||(-50<y&&y<50&&-50<x&&x<-40)||(-50<y&&y<50&&40<x&&x<50)){
-      
       seleccion[0].object.material.color.setHex(0xffffff);}
     else
       seleccion[0].object.material.color.setHex(0x00ff00);
