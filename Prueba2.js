40c990e25a2da676b2772da9d0bf775aba984a41
AngelVillalobos <langelvilla22@gmail.com>
2016-11-18 11:38:37 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -305,7 +305,7 @@ function SeleccionD(event)
   {
     x=seleccion[0].point.x;
     y=seleccion[0].point.y;
-    console.log(seleccion[0].object.parent);//==='Peon')
+    console.log(environment.children);//==='Peon')
       //var A=1;
     //console.log(environment);
     //console.log(A);
