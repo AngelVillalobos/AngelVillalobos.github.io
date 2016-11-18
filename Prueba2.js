e52e002ab24bbf717e71ec7ea9339b50bd2ed137
AngelVillalobos <langelvilla22@gmail.com>
2016-11-18 16:09:55 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -195,20 +195,19 @@ Peon.prototype.sense=function(environment)
 Peon.prototype.plan=function(environment)
 {
   this.actuator.commands=[];
-  if(this.sensor.colision===true)
-  {
-    this.actuator.commands.push('rotateCCW');
-  }
-  else
-  { 
-    if(X!==x)//&&this.sensor.colision==false)
-      this.actuator.commands.push('goStraightZ');
-      //this.actuator.commands.push('goStraightY');
-    else if(X===x&&Y!==y)//&&this.sensor.colision==false) 
+  //if(this.sensor.colision===true)
+  //{
+  //  this.actuator.commands.push('rotateCCW');
+  //}
+  //else
+  //{ 
+    if(X!==x)
+      this.actuator.commands.push('goStraightX');
+    else if(X===x&&Y!==y) 
       this.actuator.commands.push('goStraightY');
-    else if(X===x&&Y===y)//&&this.sensor.colision==false)
+    else if(X===x&&Y===y)
        this.actuator.commands.push('stop');
-  }
+  //}
 };
 
 Peon.prototype.act=function(environment)
@@ -236,20 +235,6 @@ Peon.prototype.operations.goStraightX=function(pieza,distance)
       distance=-0.5; 
   }
   pieza.position.x+=distance*Math.cos(pieza.rotation.z);
-};
-
-Peon.prototype.operations.goStraightZ=function(pieza,distance)
-{
-  if(distance===undefined)
-  {
-    if(Z<40)
-      distance=0.5;
-    else if(Z===40)
-      distance=0;
-    else
-      distance=-0.5; 
-  }
-  pieza.position.z+=distance*Math.cos(pieza.rotation.z);
 };
 
 Peon.prototype.operations.goStraightY=function(pieza,distance)
