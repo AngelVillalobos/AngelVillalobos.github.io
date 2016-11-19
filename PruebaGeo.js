cefb1197707cf4641802f8102ef07c7529cef94a
AngelVillalobos <langelvilla22@gmail.com>
2016-11-19 15:50:15 -0600

    Update PruebaGeo.js

--- PruebaGeo.js
+++ PruebaGeo.js
@@ -2,20 +2,19 @@
 ///////////////CONSTRUCTOR PEON///////////////
 PeonGeometry=function()
 {
-  var BaseRey1=new THREE.BoxGeometry(0.7,0.2,0.7);
-  var CuerpoRey1=new THREE.BoxGeometry(0.5,1.3,0.5);
-  var CoronaRey1=new THREE.BoxGeometry(0.3,0.3,0.3);
-  BaseRey1.translate(0,0,0);
-  CuerpoRey1.translate(0,0.5,0);
-  CoronaRey1.translate(0,1.3,0);
-  CoronaRey1.rotateY(Math.PI/4);
-  var BaseRey=new THREE.Mesh(BaseRey1);
-  var CuerpoRey=new THREE.Mesh(CuerpoRey1);
-  var CoronaRey=new THREE.Mesh(CoronaRey1);
-  var ReyForma = new THREE.Geometry();
-  this.merge(BaseRey.geometry,BaseRey.matrix);
-  this.merge(CuerpoRey.geometry,CuerpoRey.matrix);
-  this.merge(CoronaRey.geometry,CoronaRey.matrix);
+  var BaseReina1=new THREE.BoxGeometry(0.7,0.2,0.7);
+  var CuerpoReina1=new THREE.CylinderGeometry(0.3,0.3,1.3,32);
+  var CoronaReina1=new THREE.SphereGeometry(0.23,32,32);
+  BaseReina1.translate(0,0,0);
+  CuerpoReina1.translate(0,0.5,0);
+  CoronaReina1.translate(0,1.3,0);
+  var BaseReina=new THREE.Mesh(BaseReina1);
+  var CuerpoReina=new THREE.Mesh(CuerpoReina1);
+  var CoronaReina=new THREE.Mesh(CoronaReina1);
+  var ReinaForma = new THREE.Geometry();
+  this.merge(BaseReina.geometry,BaseReina.matrix);
+  this.merge(CuerpoReina.geometry,CuerpoReina.matrix);
+  this.merge(CoronaReina.geometry,CoronaReina.matrix);
 }
 PeonGeometry.prototype=new THREE.Geometry();
 ///////////////AGENTE///////////////
