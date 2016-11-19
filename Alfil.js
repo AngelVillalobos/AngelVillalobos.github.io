de168f78cbc243332d57f2264599e6ab6e6f96f7
AngelVillalobos <langelvilla22@gmail.com>
2016-11-19 14:03:35 -0600

    Update Alfil.js

--- Alfil.js
+++ Alfil.js
@@ -1,7 +1,7 @@
 var BaseAlfil1=new THREE.BoxGeometry(0.7,0.2,0.7);
-var CuerpoAlfil1=new THREE.ConeGeometry(0.7,1.2,4,64,2);
+var CuerpoAlfil1=new THREE.ConeGeometry(0.7,1.2,4,64,false,2);
 BaseAlfil1.translate(0,0,0);
-CuerpoAlfil1.translate(0,0.4,0);
+CuerpoAlfil1.translate(0,0.6,0);
 var BaseAlfil=new THREE.Mesh(BaseAlfil1);
 var CuerpoAlfil=new THREE.Mesh(CuerpoAlfil1);
 var AlfilForma = new THREE.Geometry();
