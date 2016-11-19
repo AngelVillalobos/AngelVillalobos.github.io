6f5986fd0e3bcbe9e88b79b80fb5ca05d1683269
AngelVillalobos <langelvilla22@gmail.com>
2016-11-19 13:25:56 -0600

    Update Peon.js

--- Peon.js
+++ Peon.js
@@ -1,7 +1,7 @@
 var BasePeon1=new THREE.BoxGeometry(0.7,0.2,0.7);
 var CuerpoPeon1=new THREE.BoxGeometry(0.5,0.8,0.5);
 BasePeon1.translate(0,0,0);
-CuerpoPeon1.translate(0,0,0);
+CuerpoPeon1.translate(0,0.4,0);
 var BasePeon=new THREE.Mesh(BasePeon1);
 var CuerpoPeon=new THREE.Mesh(CuerpoPeon1);
 var PeonForma = new THREE.Geometry();
