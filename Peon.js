6871f301458a9298947b8acfde22cbd3e220fd56
AngelVillalobos <langelvilla22@gmail.com>
2016-11-19 12:58:57 -0600

    Create Peon.js

---
+++ Peon.js
@@ -0,0 +1,22 @@
+var BasePeon1=new THREE.BoxGeometry(0.7,0.2,0.7);
+var CuerpoPeon1=new THREE.BoxGeometry(0.0.5,0.8,0.5);
+CuerpoPeon1.translate(0,0,1);
+var BasePeon=new THREE.Mesh(BasePeon1);
+var CuerpoPeon=new THREE.Mesh(CuerpoPeon1);
+var PeonForma = new THREE.Geometry();
+PeonForma.merge(BasePeon.geometry,BasePeon.matrix);
+PeonForma.merge(CuerpoPeon.geometry,CuerpoPeon.matrix);
+var material=new THREE.MeshNormalMaterial();
+var PeonFinal=new THREE.Mesh(PeonForma,material);
+//PeonFinal.rotateX(Math.PI/12);
+
+var escena = new THREE.Scene();
+escena.add(PeonFinal);
+
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=4;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
