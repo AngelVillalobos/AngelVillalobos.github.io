cab0bb0113446dd116bef7d3f2e7525af9094fc4
AngelVillalobos <langelvilla22@gmail.com>
2016-11-24 15:45:17 -0600

    Update Prueba2.js

--- Prueba2.js
+++ Prueba2.js
@@ -206,7 +206,7 @@ Peon.prototype.sense=function(environment)
   this.sensor.set(this.position,new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),1));
   var obstaculo=this.sensor.intersectObjects(environment.children,true);
   //console.log(obstaculo[0].distance)
-  if((obstaculo.length>0 && (obstaculo[0].distance<0.5)))
+  if((obstaculo.length>0 && (obstaculo[0].distance<0.742461184060705)))
   {console.log(obstaculo[0])
     this.sensor.colision=true;}
   else
