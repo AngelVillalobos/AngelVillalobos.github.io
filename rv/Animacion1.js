9c343f4d599a09774877999f0167fa89ef9ff722
AngelVillalobos <langelvilla22@gmail.com>
2016-11-25 13:37:06 -0600

    Update Animacion1.js

--- rv/Animacion1.js
+++ rv/Animacion1.js
@@ -29,7 +29,7 @@ function setup()
 }
 
 var a=0;
-var b=1;
+var b=0;
 
 function loop()
 {
@@ -40,7 +40,7 @@ function loop()
   if (a>=150)
   {
     b=-1;
-    if(a>=400)
+    if(a>=300)
     {
       a=0;
     }
