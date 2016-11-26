4d1ce82163f410c5223f395c08a32225ac0a04bb
AngelVillalobos <langelvilla22@gmail.com>
2016-11-25 20:35:48 -0600

    Update camina.js

--- camina.js
+++ camina.js
@@ -67,9 +67,9 @@ Agent.prototype.operations.goStraightX=function(pieza,distance)
   }
   pieza.position.x+=distance*Math.cos(pieza.rotation.z);
   //if(pieza.piernader.rotation.x>0.1)
-    pieza.piernader.rotation.x+=-0.5;
+    pieza.piernader.rotation.x+=-3;
   //else if(pieza.piernader.rotation.x<-0.1)
-    pieza.piernader.rotation.x+=0.5;
+    pieza.piernader.rotation.x+=3;
   //pieza.piernaizq.rotation.x+=-0.01;
 };
 
@@ -86,9 +86,9 @@ Agent.prototype.operations.goStraightY=function(pieza,distance)
   }
   pieza.position.y+=distance*Math.cos(pieza.rotation.z);
   //if(pieza.piernader.rotation.x>0.1)
-    pieza.piernader.rotation.x+=-0.5;
+    pieza.piernader.rotation.x+=-3;
   //else if(pieza.piernader.rotation.x<-0.1)
-    pieza.piernader.rotation.x+=0.5;
+    pieza.piernader.rotation.x+=3;
   //pieza.piernaizq.rotation.x+=-0.01*b;
 };
 
