273ca7b1d151cf2f9a991748bcaf8aa0c3a96356
AngelVillalobos <langelvilla22@gmail.com>
2016-11-25 23:04:57 -0600

    Update camina.js

--- camina.js
+++ camina.js
@@ -293,8 +293,8 @@ function Alfil(sTP,x,y)
   this.brazoder=new THREE.Mesh(new THREE.BoxGeometry(5,1,1),new THREE.MeshLambertMaterial({map:textura}));
   this.piernaizq.position.set(-1.8,0,0)
   this.piernader.position.set(1.8,0,0);
-  this.brazoder.position.set(1.9,0,4);
-  this.brazoizq.position.set(-1.9,0,4);
+  this.brazoder.position.set(2.1,0,4);
+  this.brazoizq.position.set(-2.1,0,4);
   this.add(this.brazoizq,this.brazoder,this.piernaizq,this.piernader,this.actuator);
   this.actuator.scale.set(9.5,9.5,9.5);
   this.actuator.rotateX(Math.PI/2);
