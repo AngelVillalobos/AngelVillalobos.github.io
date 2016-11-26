c8a42267a8d79b921eced78828be7ccbfd741c0e
AngelVillalobos <langelvilla22@gmail.com>
2016-11-25 23:11:11 -0600

    Update camina.js

--- camina.js
+++ camina.js
@@ -475,8 +475,8 @@ function setup()
   var planoLejano=1000;
   camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
   //120 -90
-  camara.position.z=120;
-  camara.position.y=-90;
+  //camara.position.z=120;
+  camara.position.y=-50;
   camara.lookAt(new THREE.Vector3(0,0,0));
   /////RENDERIZADOR////
   renderizador = new THREE.WebGLRenderer();
