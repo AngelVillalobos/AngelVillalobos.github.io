f590a240e82357d0af4f5742b248c2b7fa524dcd
AngelVillalobos <langelvilla22@gmail.com>
2016-12-05 13:49:02 -0600

    Update AjedrezF3.js

--- AjedrezF3.js
+++ AjedrezF3.js
@@ -995,7 +995,7 @@ function setup()
   var planoCercano=1;
   var planoLejano=1000;
   camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
-  //camara.position.z=120;
+  camara.position.z=120;
   camara.position.y=-90;
   camara.lookAt(new THREE.Vector3(0,0,0));
   /////RENDERIZADOR////
