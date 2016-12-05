078f309ff71f4767ffabb48594c00c79fe32f01f
AngelVillalobos <langelvilla22@gmail.com>
2016-12-05 12:50:35 -0600

    Update camina.js

--- camina.js
+++ camina.js
@@ -49,7 +49,7 @@ Agent.prototype.operations.goStraightX=function(pieza,distance)
       distance=-0.5; 
   }
   pieza.position.x+=distance*Math.cos(pieza.rotation.z);
-  pieza.rotateY(MAth.PI/2);
+  pieza.rotateY(Math.PI/2);
   b=pieza.piernader.rotation.x*Math.pow(10, 1);
   b=Math.round(b);
   b= b/Math.pow(10,1);
